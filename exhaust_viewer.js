cdd20d1d8d009874d6a3d66ad1de2fe0a84d4176
MANL <skygarlics@users.noreply.github.com>
2025-04-16 19:07:34 +0900

    bootstap version

--- exhaust_viewer.js
+++ exhaust_viewer.js
@@ -181,18 +181,11 @@ export class EXHaustViewer {
         iframe.style.zIndex = '9999';
         iframe.style.display = 'none';
 
-        // integrity problem?
-        var bs_js;
-        if (GM_getResourceText) {
-            bs_js = GM_getResourceText('bs_js');
-        } else {
-            bs_js = 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js';
-        }
-
         iframe.srcdoc = `<!DOCTYPE html><html>
             <head>
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+                <link href=" https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet">
+                <script src=" https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js "></script>
                 <script>
                     ${bs_js}
                 </script>
